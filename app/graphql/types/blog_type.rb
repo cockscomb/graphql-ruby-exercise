862bca9815bc971b828d8a6cb5d5454628ae04a5
Hiroki Kato <cockscomb@gmail.com>
2021-12-15 23:49:01 +0900

    Remove internal fields

--- app/graphql/types/blog_type.rb
+++ app/graphql/types/blog_type.rb
@@ -1,9 +1,6 @@
 module Types
   class BlogType < Types::BaseObject
     field :id, ID, null: false
-    field :owner_id, Integer, null: false
     field :name, String, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
